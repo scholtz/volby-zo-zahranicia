40d4a4814022d90993f64d145b0c9c53ae6583fb
Ludo <ludko2@gmail.com>
2019-09-09 10:52:09 +0200

    hash update

--- js/makehash.php
+++ js/makehash.php
@@ -2,8 +2,14 @@
 
 foreach(scandir(".") as $file){
     if(substr($file,-3) != ".js") continue;
-    var_dump($file);
+    if(strlen($file) > 32) {
+        //unlink($file);
+        continue;
+    }
     $md5 = md5_file($file);
     $newname = substr($file,0,-3)."-".$md5.substr($file,-3);
-    symlink($file,$newname);
+    if(!file_exists($newname)){
+        echo "Novy subor: $newname\n";
+        symlink($file,$newname);
+    }
 }
